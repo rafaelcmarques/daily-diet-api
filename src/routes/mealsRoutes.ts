8bbc37f15c98d113b1b18da90853bb5710147011
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-07-26 20:38:58 -0300

    indentação

--- src/routes/mealsRoutes.ts
+++ src/routes/mealsRoutes.ts
@@ -1,42 +1,203 @@
 import { FastifyInstance } from 'fastify'
-import { z,  } from 'zod'
+import { z } from 'zod'
 import { knex } from '../database'
 import crypto from 'node:crypto'
+import { checkUserIdExists } from '../middlewares/check-user-id'
 
-export async function mealsRoutes(app: FastifyInstance){
-
-  app.post('/', async(request, reply)=>{
-    const createMealSchema = z.object({
-      name: z.string(),
-      description: z.string(),
-      date: z.string(),
-      time: z.string(),
-      onDiet :z.boolean(),
-    })
-
-    const userId = request.cookies.userId
-    if(!userId){
-      return reply.status(401).send({
-        error: 'Unauthorized.'
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post(
+    '/',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const createMealSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        time: z.string(),
+        onDiet: z.boolean(),
       })
-    }
 
-    const { name, description, date, time, onDiet } = createMealSchema.parse(request.body)
+      const userId = request.cookies.userId
+      if (!userId) {
+        return reply.status(401).send({
+          error: 'Unauthorized.',
+        })
+      }
 
-    try{
-      await knex('meals').insert({ 
-        id: crypto.randomUUID(), 
-        name, 
-        description, 
-        date, 
-        time, 
-        onDiet, 
-        userId
+      const { name, description, date, time, onDiet } = createMealSchema.parse(
+        request.body,
+      )
+
+      try {
+        await knex('meals').insert({
+          id: crypto.randomUUID(),
+          name,
+          description,
+          date,
+          time,
+          onDiet,
+          userId,
+        })
+        return reply.status(201).send()
+      } catch (e) {
+        console.log(e)
+        return reply.status(400).send()
+      }
+    },
+  )
+
+  app.get(
+    '/',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const { userId } = request.cookies
+      try {
+        const meals = await knex('meals')
+          .where({ userId })
+          .orderBy('date')
+          .orderBy('time')
+          .select()
+        return { meals }
+      } catch (e) {
+        console.log(e)
+        return reply.status(400).send()
+      }
+    },
+  )
+
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const idSchema = z.object({
+        id: z.string().uuid(),
       })
-      return reply.status(201).send()
-    }catch(e){
-      console.log(e)
-      return reply.status(400).send()
-    }
-  })
+      const { userId } = request.cookies
+
+      const { id } = idSchema.parse(request.params)
+
+      try {
+        const meal = await knex('meals').where({ id, userId }).first()
+        return { meal }
+      } catch (e) {
+        console.log(e)
+        return reply.status(400).send()
+      }
+    },
+  )
+
+  app.get(
+    '/metrics',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const { userId } = request.cookies
+      const totalMeals = await knex('meals')
+        .where({ userId })
+        .count('* as total')
+        .first()
+      const onDiet = await knex('meals')
+        .where({ userId, onDiet: true })
+        .count('* as total')
+        .first()
+      const outDiet = await knex('meals')
+        .where({ userId, onDiet: false })
+        .count('* as total')
+        .first()
+
+      let currentStreak = 0
+      let maxStreak = 0
+
+      await knex('meals')
+        .where({ userId })
+        .orderBy('date')
+        .orderBy('time')
+        .select()
+        .then((meals) => {
+          meals.forEach((meal) => {
+            if (meal.onDiet) {
+              currentStreak++
+              if (currentStreak > maxStreak) {
+                maxStreak = currentStreak
+              }
+            } else {
+              currentStreak = 0
+            }
+          })
+        })
+
+      const metrics = {
+        totalMeals,
+        onDiet,
+        outDiet,
+        currentStreak,
+        maxStreak,
+      }
+      return reply.status(200).send(metrics)
+    },
+  )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const idSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const mealsSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        onDiet: z.boolean(),
+        date: z.string(),
+        time: z.string(),
+      })
+
+      const { id } = idSchema.parse(request.params)
+      const { userId } = request.cookies
+      const { name, description, onDiet, date, time } = mealsSchema.parse(
+        request.body,
+      )
+
+      try {
+        await knex('meals')
+          .where({ userId, id })
+          .update({ name, description, onDiet, date, time })
+        return reply.status(200).send()
+      } catch (e) {
+        return reply.status(400).send()
+      }
+    },
+  )
+
+  app.delete(
+    '/:id',
+    {
+      preHandler: [checkUserIdExists],
+    },
+    async (request, reply) => {
+      const idSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = idSchema.parse(request.params)
+      const { userId } = request.cookies
+
+      try {
+        await knex('meals').where({ userId, id }).delete()
+        return reply.status(204).send()
+      } catch (e) {
+        return reply.status(400).send()
+      }
+    },
+  )
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,42 +4,39 @@ import { knex } from '../database'
 import crypto from 'node:crypto'
 
 export async function userRoutes(app: FastifyInstance) {
-
   app.post('/', async (request, reply) => {
     const createUserSchema = z.object({
       name: z.string(),
       email: z.string(),
       password: z.string(),
-  
     })
 
     const { name, email, password } = createUserSchema.parse(request.body)
 
-    try{
-      const  id = crypto.randomUUID()
+    try {
+      const id = crypto.randomUUID()
       await knex('users').insert({
         id,
-        name, 
-        email, 
+        name,
+        email,
         password,
       })
 
       request.id = id
       return reply.status(201).send()
-    }catch(e){
+    } catch (e) {
       console.log(e)
       return reply.status(400).send()
     }
   })
-  
+
   app.addHook('onSend', (request, reply, payload, done) => {
     if (request.method === 'POST' && request.url === '/user') {
-        reply.setCookie('userId', request.id, {
+      reply.setCookie('userId', request.id, {
         path: '/',
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-      });
+      })
     }
-    done();
-  });
-  
+    done()
+  })
 }
